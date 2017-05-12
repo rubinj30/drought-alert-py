e4179eb39f74fe84f9e70cbdb71a65cf0f5da61d
rubinj30 <jonathan.a.rubin@gmail.com>
2017-05-11 21:45:28 -0400

    Update wunderground_py3.py
    Find precipation in inches for past 3 days

--- wunderground_py3.py
+++ wunderground_py3.py
@@ -1,19 +1,23 @@
+import arrow
+
+#getting dates for previous 3 days into appropriate format for url request
+days_ago_3 = arrow.utcnow().replace(days=-3).format('YYYYMMDD')
+days_ago_2 = arrow.utcnow().replace(days=-2).format('YYYYMMDD')
+days_ago_1 = arrow.utcnow().replace(days=-1).format('YYYYMMDD')
+previous_3_days = [days_ago_3,days_ago_2,days_ago_1]
+
+
+
 from urllib.request import urlopen
 import json
+
 city = "Marietta"
 state = "GA"
-date = "20170406"
-data = urlopen(f'http://api.wunderground.com/api/cdc9167d053abdfa/history_{date}/q/{state}/{city}.json')
-json_string = str(data.read(),'utf-8')
-parsed_json = json.loads(json_string)
+
+for i in previous_3_days:
+	  data = urlopen(f'http://api.wunderground.com/api/cdc9167d053abdfa/history_{i}/q/{state}/{city}.json')
+	  json_string = str(data.read(),'utf-8')
+  	parsed_json = json.loads(json_string)
 #print(json.dumps(parsed_json, indent=4, sort_keys=True)) # No need for, but i like the way it printed out so clearly
-meantempi = parsed_json['history']['dailysummary'][0]['meantempi']
-precipi = parsed_json['history']['dailysummary'][0]['precipi']
-# using ''' to print just so we can do multi-line statements. 
-string_to_print = f'''Date: {date}
-City: {city}
-Average Temperature: {meantempi} degrees Farenheit
-Total Rainfall: {precipi} inches'''
-print(string_to_print)
-data.close()
-#2nd attempt at forking
+  	precipi = parsed_json['history']['dailysummary'][0]['precipi']
+	  print(precipi)
