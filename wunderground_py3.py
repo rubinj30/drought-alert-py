145c4aedbb0285a4aee77eaf84a8689bef18030f
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2017-08-13 15:13:51 -0400

    Updating variables from camelCase to be in line with pep8 standards

--- wunderground_py3.py
+++ wunderground_py3.py
@@ -3,9 +3,9 @@ import smtplib
 from urllib.request import urlopen
 import json
 
-gmailPassword = input("What is your gmail password?")
+gmail_password = input("What is your gmail password?")
 
-#getting dates for previous 3 days into appropriate format for url request
+# getting dates for previous 3 days into appropriate format for url request
 days_ago_3 = arrow.utcnow().replace(days=-3).format('YYYYMMDD')
 days_ago_2 = arrow.utcnow().replace(days=-2).format('YYYYMMDD')
 days_ago_1 = arrow.utcnow().replace(days=-1).format('YYYYMMDD')
@@ -14,31 +14,31 @@ previous_3_days = [days_ago_3,days_ago_2,days_ago_1]
 city = "Marietta"
 state = "GA"
 
-listOf3DaysPrecip = []
+list_3_days_precip = []
 
-def pullPrecip():
+def pull_precip():
 	for i in previous_3_days:
 		data = urlopen(f'http://api.wunderground.com/api/cdc9167d053abdfa/history_{i}/q/{state}/{city}.json')
 		json_string = str(data.read(),'utf-8')
 		parsed_json = json.loads(json_string)
 		#print(json.dumps(parsed_json, indent=4, sort_keys=True)) # No need for, but i like the way it printed out so clearly
 		precipi = float(parsed_json['history']['dailysummary'][0]['precipi'])
-		listOf3DaysPrecip.append(precipi)
+		list_3_days_precip.append(precipi)
 
-pullPrecip()
+pull_precip()
 
-totalPrecip = sum(map(float,listOf3DaysPrecip))
+total_precip = sum(map(float,list_3_days_precip))
 
-if totalPrecip < 0.5:
+if total_precip < 0.5:
 	smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 	smtpObj.ehlo()
 	smtpObj.starttls()
-	smtpObj.login('my@email.com', gmailPassword)
+	smtpObj.login('my@email.com', gmail_password)
 
 	#can use list of strings for multiple email addresses. save as object and replace singular email
 	recipients = ['recipient@email.com']
 	smtpObj.sendmail('my@email.com',recipients,
-	'Subject: Remember to Wather Your Plants\nHello,\n\nIf you are receiving this e-mail, you need to water your plants. The total rainfall from the past few days is:\n' + str(totalPrecip) +' inches\n\nThat is too low, so turn on those sprinklers.')
+	'Subject: Remember to Wather Your Plants\nHello,\n\nThere has only been ' + str(totalPrecip) + ' inches of rainfall in the past few days. Make sure to water your plants.')
 
 	#disconnects from server
 	smtpObj.quit()
