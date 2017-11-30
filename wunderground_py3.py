cec8f52415e208412561f9835fc91ebc90c32f34
rubinj30 <jonathan.a.rubin@gmail.com>
2017-11-29 23:25:16 -0500

    Updated variable name to match the ones previously updated. Also, removed Python 2 file

--- wunderground_py3.py
+++ wunderground_py3.py
@@ -6,12 +6,12 @@ import json
 gmail_password = input("What is your gmail password?")
 
 # getting dates for previous 3 days into appropriate format for url request
-days_ago_3 = arrow.utcnow().replace(days=-3).format('YYYYMMDD')
-days_ago_2 = arrow.utcnow().replace(days=-2).format('YYYYMMDD')
-days_ago_1 = arrow.utcnow().replace(days=-1).format('YYYYMMDD')
-previous_3_days = [days_ago_3,days_ago_2,days_ago_1]
+days_ago_3 = arrow.utcnow().replace(days = -3).format('YYYYMMDD')
+days_ago_2 = arrow.utcnow().replace(days = -2).format('YYYYMMDD')
+days_ago_1 = arrow.utcnow().replace(days = -1).format('YYYYMMDD')
+previous_3_days = [days_ago_3, days_ago_2, days_ago_1]
 
-city = "Marietta"
+city = "Atlanta"
 state = "GA"
 
 list_3_days_precip = []
@@ -21,7 +21,6 @@ def pull_precip():
 		data = urlopen(f'http://api.wunderground.com/api/cdc9167d053abdfa/history_{i}/q/{state}/{city}.json')
 		json_string = str(data.read(),'utf-8')
 		parsed_json = json.loads(json_string)
-		#print(json.dumps(parsed_json, indent=4, sort_keys=True)) # No need for, but i like the way it printed out so clearly
 		precipi = float(parsed_json['history']['dailysummary'][0]['precipi'])
 		list_3_days_precip.append(precipi)
 
@@ -38,7 +37,7 @@ if total_precip < 0.5:
 	#can use list of strings for multiple email addresses. save as object and replace singular email
 	recipients = ['recipient@email.com']
 	smtpObj.sendmail('my@email.com',recipients,
-	'Subject: Remember to Wather Your Plants\nHello,\n\nThere has only been ' + str(totalPrecip) + ' inches of rainfall in the past few days. Make sure to water your plants.')
+	'Subject: Remember to Wather Your Plants\nHello,\n\nThere has only been ' + str(total_precip) + ' inches of rainfall in the past few days. Make sure to water your plants.')
 
 	#disconnects from server
 	smtpObj.quit()
